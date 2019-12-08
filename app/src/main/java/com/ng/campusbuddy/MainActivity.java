6ad5dab1876d2ddc6eb7787e374213ffdba354b0
Campus Buddy <campusbuddy.19@gmail.com>
2019-12-08 17:53:33 +0100

    commit

--- app/src/main/java/com/ng/campusbuddy/MainActivity.java
+++ app/src/main/java/com/ng/campusbuddy/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,15 +17,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.ng.campusbuddy.Education.EducationActivity;
 import com.ng.campusbuddy.Social.SocialActivity;
+import com.ng.campusbuddy.Tools.SliderAdapter;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class MainActivity extends AppCompatActivity {
 
     Context mcontext = MainActivity.this;
-
-    NavigationView navigationView;
-    DrawerLayout drawerLayout;
-
-    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +37,30 @@ public class MainActivity extends AppCompatActivity {
         SetupNavigationDrawer();
         
         FolatingActionButton();
+
+        ADimageslider();
+    }
+
+    private void ADimageslider() {
+
+        SliderView sliderView = findViewById(R.id.imageSlider);
+
+        SliderAdapter adapter = new SliderAdapter(this);
+
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
+        sliderView.startAutoCycle();
     }
 
     private void FolatingActionButton() {
-        
-        floatingActionButton = (FloatingActionButton) findViewById(R.id.post_fab);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.post_fab);
         
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,8 +72,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void SetupNavigationDrawer() {
 
-        drawerLayout = findViewById(R.id.drawerLayout);
-        navigationView = findViewById(R.id.nav_drawer);
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.nav_drawer);
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
--- app/src/main/java/com/ng/campusbuddy/SettingsActivity.java
+++ app/src/main/java/com/ng/campusbuddy/SettingsActivity.java
@@ -18,18 +18,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        SliderView sliderView = findViewById(R.id.imageSlider);
 
-        SliderAdapter adapter = new SliderAdapter(this);
-
-        sliderView.setSliderAdapter(adapter);
-
-        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
-        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
-        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
-        sliderView.setIndicatorSelectedColor(Color.WHITE);
-        sliderView.setIndicatorUnselectedColor(Color.GRAY);
-        sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
-        sliderView.startAutoCycle();
     }
 }
