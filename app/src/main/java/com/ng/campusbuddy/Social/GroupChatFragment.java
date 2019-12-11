71a0076630db20f6fd62abdde53c06c133aaedb3
Campus Buddy <campusbuddy.19@gmail.com>
2019-12-11 01:34:39 +0100

    Onboard Complete

---
+++ app/src/main/java/com/ng/campusbuddy/Social/GroupChatFragment.java
@@ -0,0 +1,26 @@
+package com.ng.campusbuddy.Social;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ng.campusbuddy.R;
+
+
+public class GroupChatFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group_chat, container, false);
+    }
+
+
+}
